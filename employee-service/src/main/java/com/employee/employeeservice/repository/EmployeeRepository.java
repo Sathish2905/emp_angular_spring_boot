7e2351ae3d467571cbfdcac4baa681b4176f50cc
sathish1610 <sathishkumart@logiwareinc.in>
2019-12-03 18:42:54 +0530

    first commit

---
+++ employee-service/src/main/java/com/employee/employeeservice/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.employee.employeeservice.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.employee.employeeservice.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
